a6c608fed152261aff1e8efaf1ccc4207a878c95
Spencer <williams.spencer15@gmail.com>
2019-07-31 09:42:20 -0400

    Delete config.py

--- smtp/config.py
+++
@@ -1,6 +0,0 @@
-EMAIL_SENDER ='whoissending@.com'
-PASSWORD ='password'
-EMAIL_RECIPIENT = 'whoisrecieving@.com'
-SUBJECT = 'Stepping Narrative - Small Steps Matter!' 
-CONTENT = 'Plots attached...'
-
